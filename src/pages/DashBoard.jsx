ce6d72f6714adabe3a5c07e9b77f7fe8444f50d1
H82M19\Student <Student>
2025-04-06 14:52:17 +0700

    ...

--- src/pages/DashBoard.jsx
+++ src/pages/DashBoard.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Button, Tag } from 'antd';
 import DataTable from 'react-data-table-component';
-import { ProjectOutlined } from '@ant-design/icons'
+import { ProjectOutlined, CaretUpOutlined } from '@ant-design/icons'
 const data = [
   { id: 1, customerName: 'Elizabeth Lee', company: 'AvatarSystems', orderValue: '$539', orderDate: '10/07/2023', status: 'New' },
   { id: 2, customerName: 'Carlos Garcia', company: 'SnoozeShift', orderValue: '$747', orderDate: '24/07/2023', status: 'New' },
@@ -65,34 +65,43 @@ const Dashboard = () => {
       {/* Overview Section */}
       <h2 className="font-bold text-xl flex items-center space-x-2 mb-2 pb-2">
         <ProjectOutlined className="text-blue-600" />
-        <span>Overview</span>
+        <span className='text-gray-800'>Overview</span>
       </h2>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
         <div className="bg-white p-6 rounded-lg shadow-md">
           <h3 className="text-lg font-semibold text-gray-700">Turnover</h3>
           <p className="text-3xl font-bold text-blue-600">$92,405</p>
-          <p className="text-green-500 text-sm">5.39% period of change</p>
+          <p className="text-green-500 text-sm">
+            <CaretUpOutlined className='mr-2' />
+            5.39% period of change
+          </p>
         </div>
         <div className="bg-white p-6 rounded-lg shadow-md">
           <h3 className="text-lg font-semibold text-gray-700">Profit</h3>
           <p className="text-3xl font-bold text-blue-600">$32,218</p>
-          <p className="text-green-500 text-sm">5.39% period of change</p>
+          <p className="text-green-500 text-sm">
+            <CaretUpOutlined className='mr-2' />
+            5.39% period of change
+          </p>
         </div>
         <div className="bg-white p-6 rounded-lg shadow-md">
           <h3 className="text-lg font-semibold text-gray-700">New Customer</h3>
           <p className="text-3xl font-bold text-blue-600">298</p>
-          <p className="text-green-500 text-sm">6.8% period of change</p>
+          <p className="text-green-500 text-sm">
+            <CaretUpOutlined className='mr-2' />
+            6.8% period of change
+          </p>
         </div>
       </div>
 
       {/* Detailed Report Section */}
       <h2 className="font-bold text-xl flex items-center space-x-2 mt-6 mb-2 pb-2">
         <ProjectOutlined className="text-blue-600" />
-        <span>Detail Report</span>
+        <span className='text-gray-800'>Detail Report</span>
       </h2>
 
-      <div className="bg-white p-6 rounded-lg shadow-md">
-        <div className="flex justify-between items-center mb-4">
+      {/* <div className="bg-white p-6 rounded-lg shadow-md">
+        <div className="text-end">
           <div className="space-x-2">
             <Button type="default">Import</Button>
             <Button type="primary" style={{ backgroundColor: '#2563eb', borderColor: '#2563eb' }}>
@@ -103,15 +112,15 @@ const Dashboard = () => {
         <DataTable
           columns={columns}
           data={tableData}
-          pagination
-          paginationPerPage={5}
-          paginationRowsPerPageOptions={[5, 10, 15]}
-          highlightOnHover
+          // pagination
+          // paginationPerPage={5}
+          // paginationRowsPerPageOptions={[5, 10, 15]}
+          // highlightOnHover
           customStyles={{
             headCells: {
               style: {
                 fontWeight: 'bold',
-                color: '#2563eb', // Màu chủ đạo blue-600
+                color: '#2563eb',
               },
             },
             cells: {
@@ -121,7 +130,7 @@ const Dashboard = () => {
             },
           }}
         />
-      </div>
+      </div> */}
     </div>
   );
 };
